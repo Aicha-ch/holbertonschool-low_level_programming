26ab14652473298a079f82a377bd63de625c60a9
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-16 11:03:15 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main (void)
+/**
+* main - Entery point
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts ("\"Programming is like building a multilingual puzzle");
 return(0);
 }
