0cb3c4f0a1d47afb4fc014ca3a4b84257e384048
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-19 06:37:08 -0700

    task1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,16 +3,15 @@
 #include <stdio.h>
 /* *
  * main - print the last digit of the number stored in the variable n
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 int n;
-int last_digit = n % 10;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+int last_digit = n % 10;
 if (last_digit > 0)
 printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 else if (last_digit == 0)
