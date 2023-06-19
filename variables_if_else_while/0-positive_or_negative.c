2ec8d5167d4c93ce545c5d717d37b728d695f0f5
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-19 05:28:46 -0700

    task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - funtion that prints string
+ * main - Entery point
  * return: Always 0 (Success)
  */
 int main(void)
@@ -13,10 +13,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("n is positive\n");
-else if (n = 0)
-printf("n is zero\n");
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
 else
-printf("n is negative\n");
+printf("%d is negative\n", n);
 return (0);
 }
