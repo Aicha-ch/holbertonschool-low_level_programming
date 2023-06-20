812954406b791ad324ecc6d67fe5039176b14857
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-20 03:35:24 -0700

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase, followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i;
+char x;
+for (i = 0; i <= 9; i++)
+{
+putchar (i + '0');
+}
+for (x = 'a'; x <= 'f'; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
