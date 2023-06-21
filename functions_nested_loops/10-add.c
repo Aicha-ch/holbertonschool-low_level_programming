c315094c62561bcaede0038fed844af898b58598
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-21 16:54:17 -0700

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ *@a,b: integer input
+ * Return: an integer
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
