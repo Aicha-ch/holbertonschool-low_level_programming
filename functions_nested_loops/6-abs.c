7341bbf0407bdc586d067bab5313dbb8ba7b9f88
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-21 03:45:23 -0700

    task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _abs - a function that computes the absolute value of an integer
- *
+ *@n: input integer
  * Return: No return
  */
 int _abs(int n)
@@ -9,7 +9,7 @@ int _abs(int n)
 if (n > 0)
 return (n);
 else if (n < 0)
-return ( n * (-1));
+return (n * (-1));
 else
 return (0);
 }
