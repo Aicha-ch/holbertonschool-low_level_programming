0fb45a9615f2a1926bfe46cc219c7f1cf4f0273d
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-21 01:39:21 -0700

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islowe - a function that checks for lowercase character
+ *
+ * Return: no return
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return 1;
+else
+return 0;
+}
