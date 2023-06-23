34d11a928807b3a42e6a408c5d5e1287db4139e7
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-23 01:05:31 -0700

    task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * int _isupper - a function that checks for uppercase character
+ * @c: input integer
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+c = 65;
+if (c >= 65 && c >= 90)
+return (1);
+else
+return (0);
+}
