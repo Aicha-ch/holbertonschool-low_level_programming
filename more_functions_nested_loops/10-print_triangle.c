9f927fb479923874d1924c9c0010eb7d5571a054
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-25 05:53:55 -0700

    task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - a function that prints a triangle
+ * @size: input integer
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j <= (size - i); j++)
+			{
+				_putchar (' ');
+			}
+			for (k = 0; k < i; k++)
+			{	
+			_putchar ('#');
+			}
+			_putchar ('\n');
+		}
+	else
+		_putchar ('\n');
+}
