a777f3e1021e5c1d44985f171988855ca1067ba8
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-25 07:11:41 -0700

    task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,15 +9,15 @@ void print_triangle(int size)
 	int i, j, k;
 
 	if (size > 0)
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < (size - i); j++)
+			for (j = 1; j <= (size - i); j++)
 			{
 				_putchar (' ');
 			}
-			for (k = 0; k < i; k++)
+			for (k = 1; k <= i; k++)
 			{
-			_putchar ('#');
+				_putchar ('#');
 			}
 			_putchar ('\n');
 		}
