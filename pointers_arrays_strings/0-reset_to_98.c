17c284bbc3e4c856019582a781be4810690b3c2b
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-06-26 23:09:33 -0700

    task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - a function that takes a pointer and updates the value to 98
  * @n: pointer to integer
  * Return: void
  */
