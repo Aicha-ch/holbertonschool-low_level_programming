30cf361e335378b634e213d002e49cc8bb1c8068
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-07-03 05:10:57 -0700

    task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+/** 
+ * _strstr - a function that locates a substring
+ * @haystack: pointer to character
+ * @needle: pointer to charachter
+ * Return: pointer to substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+		       if (needle[j] != haystack[i + j])
+			       break;
+		}
+		if (needle[j] == '\0')
+		return (&haystack[j + 2]);
+	}
+	return (NULL);
+}
