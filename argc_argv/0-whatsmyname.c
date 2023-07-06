b6f4309f68b4f44cda63a6943b568655920158df
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-07-05 17:32:24 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of argument passed 
+ * @argv: array pointing to arguements
+ * Return: 0 (Success)
+ */
+int main (int argc, char *argv[])
+{
+	int argc = 0
+
+		printf("%s\n", argv[argc]);
+	return (0);
+}
