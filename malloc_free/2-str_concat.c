93c9d10b466d53d3ec4198bf7fa77065765e45e7
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-07-11 08:12:56 -0700

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,15 +16,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	else if (s1 == NULL && s2 != NULL)
-	{
-		return (s2);
-	}
-	else if (s1 != NULL && s2 == NULL)
-	{
-		return (s1);
-	}
-	else
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
