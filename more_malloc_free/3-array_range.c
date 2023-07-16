d32280a54806e937d6a641e49a2d9b895acb3fe9
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-07-16 11:16:40 -0700

    task3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,20 +10,19 @@ int *array_range(int min, int max)
 {
 	int *arr;
 	int i, len;
-
+	
 	if (min > max)
 	{
 		return (NULL);
 	}
-
+	
 	len = max - min + 1;
-
 	arr = malloc(sizeof(int) * len);
 	if (arr == NULL)
-        {
-                return (NULL);
-        }
-
+	{
+		return (NULL);
+	}
+	
 	for (i = 0; i < len; i++)
 	{
 		arr[i] = min + i;
