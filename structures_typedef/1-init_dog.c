e0ff08e906a870b0b91b7e72a9170d4acccbea67
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-07-17 04:19:35 -0700

    task1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - is a functio that initialize a variable
+ * @d: pointer to struct dog
+ * @name: ppointer to character
+ * @owner: pointer to character
+ * @age: input float
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
