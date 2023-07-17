43636479820a057700a012df1def25fd0a03e195
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-07-17 02:59:27 -0700

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _dog_h_
+#define _dog_h_
+/**
+ * struct dog: structurr
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
