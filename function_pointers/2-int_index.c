b80c89eee07c90468a32b23b5b140747618a327d
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-07-18 18:32:25 -0700

    function that searches for an integer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,10 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
+	if (cmp == NULL)
+	{
+		return (-1);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
-	return (i);
+	return (-1);
 }
