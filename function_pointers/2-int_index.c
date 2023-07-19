dd29aeee710b4e51ce8466f809b0cdf4ca89aa4a
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-07-18 18:27:12 -0700

    function that searches for an integer

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - is a function that searches for an integer
+ * @array: is the array of integers
+ * @size: is the size of the array
+ * @cmp: pointer to function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (i);
+}
