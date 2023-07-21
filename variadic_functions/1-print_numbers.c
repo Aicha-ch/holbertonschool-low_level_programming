5eef66f41c162e267c1cc352767e957bf9d76b6f
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-07-21 11:12:09 -0700

    task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - is a  function that prints numbers
+ * @n: inputs parameters
+ * @separator: input character
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	if (separator == NULL)
+	{
+		return;
+	}
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, const unsigned int));
+	}
+
+	if (i != (n - 1))
+	{
+		printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(args);
+}
