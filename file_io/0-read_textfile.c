6517067ff6913ac83b0a499ef471508d21b2d300
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-08-08 15:23:15 -0700

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,24 +12,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	int nl;
 	char *buf;
-	
+
 	if (filename == NULL)
 		return (0);
-	
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	
+
 	buf = malloc(letters * sizeof(char));
 	if (buf == NULL)
 		return (0);
-	
+
 	nl = read(fd, buf, letters);
 	if (write(STDOUT_FILENO, buf, nl) != nl)
 		return (0);
-	
+
 	close(fd);
 	free(buf);
-	
+
 	return (nl);
 }
