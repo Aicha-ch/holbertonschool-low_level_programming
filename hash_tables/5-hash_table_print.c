5959515f41d32a4b8d0716e72eea3680344bc316
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-08-11 09:33:04 -0700

    Write a function that prints a hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Write a function that prints a hash table.
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	char vir = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (vir == 1)
+				printf(", ");
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			vir = 1;
+		}
+	}
+	printf("}\n");
+}
