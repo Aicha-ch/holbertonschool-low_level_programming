74ef305b85378310b3c0d40866da189551f6eca8
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-08-14 00:51:44 -0700

    task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -7,4 +7,19 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * struct print - definition of structure
+ *
+ * @c: char
+ * @f: function
+ */
+typedef struct print
+{
+	char *c;
+	void (*f)(va_list args);
+}print_t;
+
 #endif
