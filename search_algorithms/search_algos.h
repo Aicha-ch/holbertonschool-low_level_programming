a75b19d65baa6588dfb3749de2a6b2d7722fd8b7
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-09-20 14:55:25 -0700

    creating a function that searches for a value in an array of integers using the Linear search algorithm

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_ALGO_
+#define _SEARCH_ALGO_
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
